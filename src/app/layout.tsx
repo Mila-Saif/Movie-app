ece6173ae465cc4f96157ebd1f82784854adeed3
Mila-Saif <milaafawzi@gmail.com>
2026-03-05 06:05:51 +0300

    add a top loader action to homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SearchBar from "@/components/ui/SearchBar";
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={`${inter.className} min-h-screen flex flex-col`}>
+        <NextTopLoader
+          color="#e11d48"         
+          initialPosition={0.08}  
+          crawlSpeed={200}      
+          height={3}              
+          crawl={true}            
+          showSpinner={false}     
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #e11d48,0 0 5px #e11d48" 
+        />
         <header className="w-full border-b border-border bg-background/95 py-4">
         <div className="container mx-auto px-4 md:px-8 flex flex-col md:flex-row items-center justify-between gap-4">
           <a href="/" className="text-2xl font-extrabold text-primary tracking-wider uppercase">
